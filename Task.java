18509853a6ec8db46cb6ff8fa843d46ea53f15c1
Phandastick <ngjianbin11@gmail.com>
2024-03-01 00:18:48 +0800

    Start structure

---
+++ Task.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+/**
+ * Task
+ */
+public class Task {
+
+    private String title, description, color;
+    private LocalDate dueDate;
+    private Boolean iscompleted;
+
+    public Task(){
+        
+    }
+}
---
+++ TasksList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class TasksList {
+    private ArrayList<Task> tasks;
+
+    public void getTasks() {
+        
+    }
+}
